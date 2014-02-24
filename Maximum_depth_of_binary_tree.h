bc53bb8c66f3d50678f45e334350bb5f3c64186a
hubert_he <hubert_he@DSLDev.example.org>
2014-02-24 18:06:05 +0800

    sametree

--- Maximum_depth_of_binary_tree.h
+++ Maximum_depth_of_binary_tree.h
@@ -1,28 +1,11 @@
+#ifndef Maximum_depth_of_binary_tree_H
+#define Maximum_depth_of_binary_tree_H
 struct TreeNode
 {
 	int val;
 	TreeNode *left;
 	TreeNode *right;
 	TreeNode(int x): val(x), left(0), right(0) {}
-};
-
-class Solution
-{
-	public:
-		int maxDepth(TreeNode *root)
-		{
-			int max0 = 0, max1 = 0;
-			if(root == 0) return -1;
-			if(root->left)
-			{
-				max0 = maxDepth(root->left);
-			}
-			if(root->right)
-			{
-				max1 = maxDepth(root->right);
-			}
-			return ( (max0>max1)? max0+1: max1+1);
-		}
 };
 
 class BinTree
@@ -36,3 +19,4 @@ class BinTree
 		BinTree(char *formate);
 		TreeNode * getTree() { return tree; }
 };
+#endif
--- main.c
+++ main.c
@@ -1,10 +1,29 @@
 #include "Maximum_depth_of_binary_tree.h"
+#include "sametree.h"
 #include <iostream>
 int main()
 {
+#ifdef Max_Depth
 	BinTree aaa("abc@@d@@e@fg@@h@@");
 	Solution bb;
 	int ret = bb.maxDepth(aaa.getTree());
 	std::cout << "xxx = " << ret << std::endl;
+#endif
+#ifdef Simple_Tree
+	BinTree aaa("abc@@d@@e@fg@@h@@");
+	BinTree ccc("abc@@d@@e@fg@@h@@");
+	BinTree ddd("abd@@d@@e@fg@@h@@");
+	BinTree eee("abc@@d@@e@fg@@m@@");
+	BinTree fff("abc@@de@@@f@@");
+	Solution xx;
+	int ret = xx.isSameTreebyStack(aaa.getTree(), ccc.getTree());
+	std::cout << "\n++ret0 = " << ret << std::endl;
+	ret = xx.isSameTreebyStack(aaa.getTree(), ddd.getTree());
+	std::cout << "\n++ret0 = " << ret << std::endl;
+	ret = xx.isSameTreebyStack(aaa.getTree(), eee.getTree());
+	std::cout << "\n++ret0 = " << ret << std::endl;
+	ret = xx.isSameTreebyStack(aaa.getTree(), fff.getTree());
+	std::cout << "\n++ret0 = " << ret << std::endl;
+#endif
 	return 0;
 }
