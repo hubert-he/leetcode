e5d178cdda4c609379c28f675bce2ce102c46853
zhenhua he <hezh@fibodt.com>
2021-04-22 18:18:20 +0800

    kind of stack problem

--- stack/stack.go
+++ stack/stack.go
@@ -1,1 +1,30 @@
 package stack
+
+func ReverseInPlace(stack *[]int) {
+	if len(*stack) == 0 || len(*stack) == 1{
+		return
+	}
+	var addTop func(int)
+	addTop = func(topValue int){
+		if len(*stack) == 0{
+			*stack = append(*stack, topValue)
+			return
+		}
+		top := (*stack)[0]
+		(*stack) = (*stack)[1:]
+
+	}
+	var dfs func() int
+	dfs = func() int {
+		top := (*stack)[0]
+		if len(*stack) == 1{
+			(*stack)=(*stack)[1:]
+			return top
+		}
+		bottom := dfs()
+		(*stack) = append([]int{bottom}, (*stack)...)
+		return bottom
+	}
+	dfs()
+	return
+}
--- stack/stack_test.go
+++ stack/stack_test.go
@@ -1,1 +1,27 @@
 package stack
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestReverseInPlace(t *testing.T){
+	for caseId, testCase := range []struct {
+		stack	[]int
+		want 	[]int
+	}{
+		{[]int{},[]int{}},
+		{[]int{1}, []int{1}},
+		{[]int{1,2}, []int{2,1}},
+		{[]int{1,2,3,4,5}, []int{5,4,3,2,1}},
+	}{
+		result := make([]int, len(testCase.want))
+		copy(result, testCase.stack)
+		ReverseInPlace(&result)
+		ok := assert.Equal(t, testCase.want, result, fmt.Sprintf("case-%d Failed: result=>%v but want=>%v", caseId, result, testCase.want))
+		if !ok {
+			break
+		}
+	}
+}
