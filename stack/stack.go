6ebda16e83d983096adebe4a1ae4fb4a16a44ae0
zhenhua he <hezh@fibodt.com>
2021-04-23 17:29:12 +0800

    add stack imp using go

--- stack/stack.go
+++ stack/stack.go
@@ -5,26 +5,97 @@ func ReverseInPlace(stack *[]int) {
 		return
 	}
 	var addTop func(int)
-	addTop = func(topValue int){
+	addTop = func(bottom int){
+		top := (*stack)[0]
+		(*stack)=(*stack)[1:]
 		if len(*stack) == 0{
-			*stack = append(*stack, topValue)
+			(*stack) = append((*stack), top, bottom)
 			return
 		}
-		top := (*stack)[0]
-		(*stack) = (*stack)[1:]
-
+		addTop(bottom)
+		(*stack) = append([]int{top}, (*stack)...) // push
 	}
 	var dfs func() int
-	dfs = func() int {
+	dfs = func() int{
 		top := (*stack)[0]
-		if len(*stack) == 1{
-			(*stack)=(*stack)[1:]
+		(*stack)=(*stack)[1:]
+		if len(*stack) == 0{
+			(*stack) = append(*stack, top)
 			return top
 		}
 		bottom := dfs()
-		(*stack) = append([]int{bottom}, (*stack)...)
+		addTop(top)
 		return bottom
 	}
 	dfs()
 	return
+}
+
+type Stack struct{
+	array []interface{}
+}
+
+func New() Stack {
+	return Stack{}
+}
+func Construct(a []interface{}) Stack{
+	return Stack{array: a}
+}
+
+func (st *Stack) Push(value interface{}){
+	st.array = append([]interface{}{value}, st.array...)
+}
+
+func (st *Stack) Pop() (value interface{}) {
+	if st.Empty(){
+		return nil
+	}
+	value = st.array[0]
+	st.array = st.array[1:]
+	return
+}
+
+func (st Stack) Top() (value interface{}) {
+	if st.Empty(){
+		return
+	}
+	value = st.array[0]
+	return
+}
+
+func (st Stack) Empty()bool{
+	if len(st.array) > 0{
+		return false
+	}else {
+		return true
+	}
+}
+
+func (st *Stack) Clear(){
+	st.array = []interface{}{}
+}
+
+func(st *Stack) Reverse() {
+	var dfs func()
+	var top2bottom func(interface{})
+	top2bottom = func(value interface{}){
+		top := st.Pop()
+		if st.Empty(){
+			st.Push(value)
+			st.Push(top)
+			return
+		}
+		top2bottom(value)
+		st.Push(top)
+	}
+	dfs = func(){
+		top := st.Pop()
+		if st.Empty(){
+			st.Push(top)
+			return
+		}
+		dfs()
+		top2bottom(top)
+	}
+	dfs()
 }
--- stack/stack_test.go
+++ stack/stack_test.go
@@ -25,3 +25,87 @@ func TestReverseInPlace(t *testing.T){
 		}
 	}
 }
+
+func TestStack_Reverse(t *testing.T) {
+	for caseId, testCase := range []struct {
+		array	[]interface{}
+		want 	[]interface{}
+	}{
+		{[]interface{}{},[]interface{}{nil}},
+		{[]interface{}{1}, []interface{}{1}},
+		{[]interface{}{1,2}, []interface{}{2,1}},
+		{[]interface{}{1,2,3,4,5}, []interface{}{5,4,3,2,1}},
+	}{
+		st := Construct(testCase.array)
+		st.Reverse()
+		ok := assert.Equal(t, testCase.want, st.array,
+			fmt.Sprintf("case-%d Failed: result=>%v but want=>%v", caseId, st.array, testCase.want))
+		if !ok {
+			break
+		}
+	}
+}
+
+func TestStack_Push(t *testing.T) {
+	for caseId, testCase := range []struct{
+		arr		[]interface{}
+		want 	[]interface{}
+	}{
+		{[]interface{}{1}, []interface{}{1}},
+		{[]interface{}{1,2}, []interface{}{2,1}},
+		{[]interface{}{1,2,3,4,5}, []interface{}{5,4,3,2,1}},
+	}{
+		st := New()
+		for _, tc := range testCase.arr{
+			st.Push(tc)
+		}
+		assert.Equal(t, testCase.want, st.array, fmt.Sprintf("case-%d Failed: array=%v shoule be %v", caseId, st.array, testCase.want))
+	}
+}
+
+func TestStack_Pop(t *testing.T) {
+	for caseId, testCase := range []struct{
+		arr		[]interface{}
+		times 	int
+		want 	[]interface{}
+	}{
+		{[]interface{}{1}, 1, []interface{}{}},
+		{[]interface{}{1,2}, 1, []interface{}{2}},
+		{[]interface{}{1,2}, 3, []interface{}{}},
+		{[]interface{}{1,2,3,4,5}, 3, []interface{}{4,5}},
+	}{
+		st := Construct(testCase.arr)
+		for i := 0; i < testCase.times; i++{
+			st.Pop()
+		}
+		assert.Equal(t, testCase.want, st.array, fmt.Sprintf("case-%d Failed: array=%v shoule be %v", caseId, st.array, testCase.want))
+	}
+}
+
+func TestStack_Empty(t *testing.T) {
+	for caseId, testCase := range []struct{
+		arr		[]interface{}
+		want 	bool
+	}{
+		{[]interface{}{1}, false},
+		{[]interface{}{1,2}, false},
+		{[]interface{}{}, true},
+	} {
+		st := Construct(testCase.arr)
+		assert.Equal(t, testCase.want, st.Empty(), fmt.Sprintf("case-%d failed: shoule be %t", caseId, testCase.want))
+	}
+}
+
+func TestStack_Top(t *testing.T) {
+		for caseId, testCase := range []struct{
+			arr		[]interface{}
+			want 	interface{}
+		}{
+			{[]interface{}{1}, 1},
+			{[]interface{}{2,1}, 2},
+			{[]interface{}{}, nil},
+		}{
+			st := Construct(testCase.arr)
+			assert.Equal(t, testCase.want, st.Top(), fmt.Sprintf("case-%d failed: top=%v shoule be %v", caseId, st.Top(), testCase.want))
+		}
+}
